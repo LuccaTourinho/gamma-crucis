23cf59cd8c2df4d108f8b63aab58af527b5dda95
Lucca Tourinho <64231338+LuccaTourinho@users.noreply.github.com>
2024-10-03 20:33:53 -0300

    Update Trees.tsx

--- src/components/Trees.tsx
+++ src/components/Trees.tsx
@@ -4,19 +4,12 @@ import React from 'react'
 import { useSupression } from '@/lib/Supression'
 
 export default function Trees() {
-  const { trees, wood, updateTrees} = useSupression();
+  const { trees, wood} = useSupression();
 
-  const handleTreeClick = () => {
-    if (trees === 0) return;
-
-    updateTrees(trees - 1); 
-    updateWood(wood + 1);   
-    
-
-  };
+  
 
   return (
-    <div className='w-full h-full hover:cursor-pointer' onClick={handleTreeClick}>
+    <div className='w-full h-full hover:cursor-pointer' >
       <div className="grid grid-cols-5 gap-4 w-full h-full p-2">
         {Array.from({ length: trees }).map((_, index) => (
           <div key={index} className="flex items-center justify-center col-span-1">
