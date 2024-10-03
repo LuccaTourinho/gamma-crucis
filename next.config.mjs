9ff72c720d3f0d17ba93cbad4b65bd4c8f1838fa
Lucca Tourinho <64231338+LuccaTourinho@users.noreply.github.com>
2024-10-03 18:27:24 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,  // Ignora erros do ESLint durante o processo de build
+  },
+};
 
 export default nextConfig;
