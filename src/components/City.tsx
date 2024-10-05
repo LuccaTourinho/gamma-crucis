f106d659112eae56895dfc40f184a9fb6ff0740d
Augusto-Rauedys-Carneiro <rauedyscarneiro@gmail.com>
2024-10-05 08:44:11 -0300

    Sou eu de novo

--- src/components/City.tsx
+++ src/components/City.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import Houses from './Houses'
 import Roads from './Roads'
-// Estou aqui de novo
-//Bom dia
+// Ola
 export default function City() {
   return (
     <div className='flex bg-white h-[70%]'>
