5a80b0e706275148135b8073154101ddf67413cd
Augusto-Rauedys-Carneiro <rauedyscarneiro@gmail.com>
2024-10-05 08:34:25 -0300

    Fazendo testes, fica tranquilo

--- src/components/City.tsx
+++ src/components/City.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Houses from './Houses'
 import Roads from './Roads'
-
+// Estou aqui de novo
+//Bom dia
 export default function City() {
   return (
     <div className='flex bg-white h-[70%]'>
