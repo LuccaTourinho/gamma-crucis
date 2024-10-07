cf4fc32a87ea5a95acfd54de7711d06421c27b65
Augusto-Rauedys-Carneiro <rauedyscarneiro@gmail.com>
2024-10-06 21:51:33 -0300

    +fonte

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,20 +65,20 @@ export default function Home() {
                !showGuide ? 
                 (
                   <div className='flex flex-col items-center justify-around w-[1440px] h-full bg-green-600 p-6 rounded-xl'>
-                     <h1 className="text-md font-bold text-white">
+                     <h1 className="text-3xl font-bold text-white">
                       GLOBE CITY
                     </h1> 
                     <div className="relative w-full h-[100px] sm:h-[120px] md:h-[130px] lg:h-[150px] xl:h-[200px] 2xl:h-[250px]">
                     <Image src={LogoMarca} alt="Globe City" fill className="object-contain"/>
                     </div>
                     <div className="flex flex-col justify-between gap-9 items-center w-full p-6">
-                      <button onClick={startGame} className="text-md font-bold text-white border-2 border-white bg-cyan-500 p-3 rounded-lg hover:cursor-pointer hover:scale-105 hover:bg-cyan-300 transition-all duration-300 w-[150px]">
+                      <button onClick={startGame} className="text-xl font-bold text-white border-2 border-white bg-cyan-500 p-3 rounded-lg hover:cursor-pointer hover:scale-105 hover:bg-cyan-300 transition-all duration-300 w-[150px]">
                         Start
                       </button>
-                      <button onClick={startGuide}  className="text-md font-bold text-white border-2 border-white bg-cyan-500 p-3 rounded-lg hover:cursor-pointer hover:scale-105 hover:bg-cyan-300 transition-all duration-300 w-[150px]">
+                      <button onClick={startGuide}  className="text-xl font-bold text-white border-2 border-white bg-cyan-500 p-3 rounded-lg hover:cursor-pointer hover:scale-105 hover:bg-cyan-300 transition-all duration-300 w-[150px]">
                         Guide
                       </button>
-                      <button onClick={startCredits} className="text-md font-bold text-white border-2 border-white bg-cyan-500 p-3 rounded-lg hover:cursor-pointer hover:scale-105 hover:bg-cyan-300 transition-all duration-300 w-[150px]">
+                      <button onClick={startCredits} className="text-xl font-bold text-white border-2 border-white bg-cyan-500 p-3 rounded-lg hover:cursor-pointer hover:scale-105 hover:bg-cyan-300 transition-all duration-300 w-[150px]">
                         Credits
                       </button>
                     </div>
@@ -88,7 +88,7 @@ export default function Home() {
                 </div>
                 ) : (
                   <div className='flex flex-col items-center justify-start w-[1440px] h-full bg-green-600 p-6 rounded-xl overflow-y-auto max-h-screen'>
-                    <p className="text-white whitespace-pre-wrap text-justify">
+                    <p className="text-white whitespace-pre-wrap text-justify text-xl">
                       GUIDE:{"\n\n"}
                       Our challenge was to create a GLOBE Program game to help players understand the world around them,
                       build awareness about one or more local or global environmental topics (e.g. urbanization, water pollution,
@@ -110,14 +110,14 @@ export default function Home() {
             ) :
             (
               <div className='flex flex-col items-center justify-around w-[1440px] h-full bg-green-600 p-6 rounded-xl'>
-                <p className="text-xs font-bold text-white">
+                <p className="text-xl font-bold text-white">
                 This project was developed by the Gamma Crucis team for the NASA Space Apps 2024 competition in Salvador-Bahia-Brazil.
                 </p>
-                <h1 className="text-xs font-bold text-white text-center">
+                <h1 className="text-xl font-bold text-white text-center">
                   Members:
                 </h1>
                 {credits.map((credit, index) => (
-                  <p key={index} className="text-xs font-bold text-white">{credit}</p>
+                  <p key={index} className="text-xl font-bold text-white">{credit}</p>
                 ))}
                 <div className="w-full h-[20%] flex flex-row">
                   <div className="relative w-full h-full">
