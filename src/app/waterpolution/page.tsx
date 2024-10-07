f1a87e4480779829bcb4cbd3e74fefd26ddf05dc
Augusto-Rauedys-Carneiro <rauedyscarneiro@gmail.com>
2024-10-07 00:46:31 -0300

    ++fontwater

--- src/app/waterpolution/page.tsx
+++ src/app/waterpolution/page.tsx
@@ -32,13 +32,14 @@ export default function Page() {
         !showGame
           ? (
             <div className='flex flex-col items-center justify-center w-[1440px] h-full bg-green-600 p-6 rounded-xl'>
-              <p className='text-white whitespace-pre-wrap text-justify'>
+              <p className='text-2xl text-white whitespace-pre-wrap text-justify'>
               Your city is going through a financial crisis at the city hall and you, {'\n'}
               as the manager, will have to decide whether or not to cut spending at the {'\n'}
               water treatment plant for the next 365 days. {'\n'}
-              YOUR CHOICES HAVE CONSEQUENCES{'\n'}
+              <span className='font-bold text-yellow-300'>YOUR CHOICES HAVE CONSEQUENCES !{'\n'}</span>
+
               </p>
-              <button onClick={startGame} className='w-[200px] h-[50px] bg-cyan-500 hover:bg-cyan-300 text-white rounded-md mt-10 flex items-center justify-center'>
+              <button onClick={startGame} className='text-2xl w-[200px] h-[50px] bg-cyan-500 hover:bg-cyan-300 text-white rounded-md mt-10 flex items-center justify-center'>
                 Play
               </button>
             </div>
